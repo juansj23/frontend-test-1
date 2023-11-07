33aed09e7c71fc3c23027c3de9856b116a3eacbd
juansj23 <jcsj24@gmail.com>
2023-11-07 17:43:14 +0000

    Primera parte maquetacion

--- prueba/src/App.js
+++ prueba/src/App.js
@@ -6,16 +6,53 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <div className="card" style={{ width: '18rem' }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+      <div className="Header">
+        <h1>Seleccione moneda</h1>
+        <p>Para la correcta medición es necesario el uso de una moneda en el centro de la palma de tu mano.</p>
+      </div>
+      <div className="row p-1">
+        <div className="col-12">
+          <p>Puedes usar una de estas monedas</p>
+          <div className="p-1 border rounded"><img src="" alt="img monedas" /></div>
+          <div className="p-1">
+            <p>Coloca la moneda en el centro de la palma de tu mano</p>
+            <div className="p-1 border rounded"><img src="" alt="img palma de la mano" /></div>
+          </div>
+        </div>
+        <div className="col-12 mt-1">
+          <p>¿Cómo te gusta llevar el anillo?</p>
+          <div className="p-1 border">
+            <div className="form-check">
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Ajustado
+              </label>
+              <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+            </div>
+          </div>
+          <button className="btn border border-danger">
+            <div className="form-check" >
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Suelto
+              </label>
+              <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className="mt-5">
+        <nav aria-label="Page navigation example">
+          <ul className="pagination justify-content-center">
+            <li className="page-item disabled">
+              <a className="page-link">Volver</a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">Siguiente</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="p-1">
+          <p>Powered by</p>
+          <img src="" alt="Logo Aitaca" />
         </div>
       </div>
 
