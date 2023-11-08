f5fb59595e6b9ee318ec76e54cf0d4569dcc2c73
Juan Silva <123385530+juansj23@users.noreply.github.com>
2023-11-08 19:02:38 +0000

    lista maquetacion faltan imagenes y colores

--- prueba/src/App.js
+++ prueba/src/App.js
@@ -6,51 +6,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <div className="Header">
-        <h1>Seleccione moneda</h1>
-        <p>Para la correcta medición es necesario el uso de una moneda en el centro de la palma de tu mano.</p>
+      <div className="col-12 text-center ">
+        <h2 className="fw-semibold">Selecciona moneda</h2>
+        <div className="col-10 text-center mx-auto">
+          <p className="text-body-secondary">Para la correcta medición es necesario el uso de una moneda en el centro de la palma de tu mano.</p>
+        </div>
       </div>
-      <div className="row p-1">
-        <div className="col-12">
-          <p>Puedes usar una de estas monedas</p>
+      <div className="row d-grid gap-1 col-10 mx-auto">
+        <div className="col-12 text-start">
+          <p className="mb-0">Puedes usar una de estas monedas</p>
           <div className="p-1 border rounded"><img src="" alt="img monedas" /></div>
-          <div className="p-1">
-            <p>Coloca la moneda en el centro de la palma de tu mano</p>
+          <div className="mt-2">
+            <p className="mb-0">Coloca la moneda en el centro de la palma de tu mano</p>
             <div className="p-1 border rounded"><img src="" alt="img palma de la mano" /></div>
           </div>
         </div>
-        <div className="col-12 mt-1">
-          <p>¿Cómo te gusta llevar el anillo?</p>
-          <div className="p-1 border">
-            <div className="form-check">
-              <label className="form-check-label" htmlFor="flexCheckDefault">
+        <div className="col-11 mt-3 text-start">
+          <p className="mb-0">¿Cómo te gusta llevar el anillo?</p>
+          <button type="button" className=" btn d-grid gap-1 col-12 mx-auto border">
+            <div className="form-check d-flex justify-content-between  p-1">
+              <label className="form-check-label fw-semibold" htmlFor="flexCheckDefault">
                 Ajustado
               </label>
               <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
             </div>
-          </div>
-          <button className="btn border border-danger">
-            <div className="form-check" >
-              <label className="form-check-label" htmlFor="flexCheckDefault">
+          </button>
+          <button type="button" className=" btn d-grid gap-2 col-12 mx-auto border mt-2">
+            <div className="form-check d-flex justify-content-between p-1">
+              <label className="form-check-label fw-semibold" for="flexCheckDefault2" >
                 Suelto
               </label>
-              <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+              <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault2" />
             </div>
           </button>
         </div>
       </div>
       <div className="mt-5">
-        <nav aria-label="Page navigation example">
-          <ul className="pagination justify-content-center">
-            <li className="page-item disabled">
-              <a className="page-link">Volver</a>
-            </li>
-            <li className="page-item">
-              <a className="page-link" href="#">Siguiente</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="p-1">
+        <div className="col-12 d-flex justify-content-center">
+            <nav className="nav nav-pills">
+              <button className="btn border m-2">
+                <a className="nav-link" aria-current="page" href="#">
+                  Volver
+                </a>
+              </button>
+              <button className="btn border m-2">
+                <a className="nav-link" href="#">
+                  Siguiente
+                </a></button>
+            </nav>
+        </div>
+        <div className="p-1 border-top border-2 mt-4">
           <p>Powered by</p>
           <img src="" alt="Logo Aitaca" />
         </div>
